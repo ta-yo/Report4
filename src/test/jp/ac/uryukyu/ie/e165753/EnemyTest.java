18c183f3ebf052c588da6ac14a57dc1449506b2c
yoshiki tanahara <e165753@ie.u-ryukyu.ac.jp>
2016-11-27 01:46:07 +0900

    初期コード

--- src/test/jp/ac/uryukyu/ie/e165753/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165753/EnemyTest.java
@@ -5,20 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EnemyTest {
-    /**
-     * 「死んでる状態では攻撃できない」ことを確認。
-     * @throws Exception
-     */
+    
     @Test
     public void attack() throws Exception {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
-        for(int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.hitPoint);
     }
+
 
 }
