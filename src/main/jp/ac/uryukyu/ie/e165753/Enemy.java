60a35c88eca6a9bc35eb6f26d59769c2ebbdc2be
yoshiki tanahara <e165753@ie.u-ryukyu.ac.jp>
2016-11-28 23:40:53 +0900

    初期コード

--- src/main/jp/ac/uryukyu/ie/e165753/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165753/Enemy.java
@@ -42,25 +42,12 @@ public class Enemy {
         return name;
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     *
-     * @param hero 攻撃対象
-     */
     public void attack(Hero hero) {
-        if (dead == false) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
-        }
     }
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     *
-     * @param damage 受けたダメージ
-     */
+   
     public void wounded(int damage) {
         hitPoint -= damage;
         if (hitPoint < 0) {
